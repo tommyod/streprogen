cbd9821e0e5512ee809671e2d0d25b77fc8444af
tommyod <tommy.odland@gmail.com>
2019-08-17 13:41:40 +0200

    condition black on python version

--- setup.py
+++ setup.py
@@ -71,12 +71,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=[
-        "jinja2>=2.10.1",
-        "ortools>=7.2.6977",
-        "pytest>=5.0.1",
-        "black==19.3b0",
-    ],
+    install_requires=["jinja2>=2.10.1", "ortools>=7.2.6977"],
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
     # for example:
