a6027b228b787088846c50458438a11648a19ce8
tommyod <tommy.odland@gmail.com>
2020-07-11 08:26:07 +0200

    deprecated py35

--- setup.py
+++ setup.py
@@ -54,9 +54,9 @@ setup(
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     # What does your project relate to?
     keywords="strength training program health fitness generator "
@@ -68,12 +68,12 @@ setup(
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
     #   py_modules=["my_module"],
-    python_requires=">=3.5",
+    python_requires=">=3.6",
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=["jinja2>=2.10.1", "ortools>=7.2.6977"],
+    install_requires=["jinja2>=2.11.2", "ortools>=7.7.7810"],
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
     # for example:
