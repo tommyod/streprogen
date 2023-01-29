7870526b488f6abd91986be5418c2b3b6cb05f76
tommyod <tommy.odland@gmail.com>
2023-01-29 12:55:01 +0100

    updated setup.py versions

--- setup.py
+++ setup.py
@@ -54,12 +54,14 @@ setup(
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
     # What does your project relate to?
-    keywords="strength training program health fitness generator " "weightlifting powerlifting exercise",
+    keywords="strength training program health fitness generator weightlifting powerlifting exercise",
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
     packages=find_packages(".", exclude=["contrib", "docs", "tests"]),
